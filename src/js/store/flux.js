02d97d99ef5b45f8a9281f2cb2673b94543dc09e
Jorge-GD <jgalvezdominguez@gmail.com>
2021-08-04 10:38:30 +0000

    Añadido fecht planets

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,42 +1,23 @@
+const URL_BASE = "https://www.swapi.tech/api/";
+
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			characters: [],
+			starship: [],
+			planets: []
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			getPlanets: () => {
+				fetch(URL_BASE.concat("planets"))
+					.then(res => {
+						if (!res.ok) {
+							throw new Error("Algo ha ido mal");
+						}
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+						return res.json();
+					})
+					.then(jsonPlanets => console.log(jsonPlanets));
 			}
 		}
 	};
